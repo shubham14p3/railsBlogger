cdc95852423cb7e72db2ed83a6f5a555f3d28f05
shubham14p3 <shubham14p3@gmail.com>
2020-05-13 02:03:29 +0530

    Adding Files

--- db/migrate/20200512201912_create_articles.rb
+++ db/migrate/20200512201912_create_articles.rb
@@ -1,8 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
-
-      t.timestamps
-    end
+		t.string :title
+		t.text :body
+	
+		t.timestamps
+	end
   end
 end
