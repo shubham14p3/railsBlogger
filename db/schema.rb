9c6abbc2ce35b5839fcba49093a6eaf4009cd41b
shubham14p3 <shubham14p3@gmail.com>
2020-05-13 02:20:33 +0530

    Testing the Article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_201912) do
+ActiveRecord::Schema.define(version: 2020_05_12_204514) do
+
+  create_table "article", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
